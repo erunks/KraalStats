ebfdd7e1f7679a4b1417463f1435534fa2e2516a
Edward Runkel <eddierunkel@gmail.com>
2018-12-18 01:36:35 -0500

    # This is a combination of 2 commits. # The first commit's message is:
    Initial steps towards a working bot
    
    # This is the 2nd commit message:
    
    Add a npm command

---
+++ app/config.js
@@ -0,0 +1,12 @@
+let config;
+
+try {
+	config = require('./config.json');
+} catch(e) {
+	config = {
+		"DISCORD_BOT_SECRET": os.environ["DISCORD_BOT_SECRET"],
+		"PAPERTRAIL_API_TOKEN": os.environ["PAPERTRAIL_API_TOKEN"]
+	}
+}
+
+module.exports = config;
