93b243131f04594d10dca18885230b6ca43b870b
Edward Runkel <eddierunkel@gmail.com>
2019-01-13 06:16:47 -0500

    Knex integration complete.

--- app/DatabaseClient.js
+++ app/DatabaseClient.js
@@ -2,53 +2,82 @@
 
 class DatabaseClient {
 	constructor() {
-		const mysql = require('mysql');
-		this.messages = require('./messages.json');
-		this.pool = mysql.createPool({
-			connectionLimit : 10,
-			host			: process.env.GEARHOST_HOST_URL,
-			user			: process.env.GEARHOST_USER_NAME,
-			password		: process.env.GEARHOST_USER_PASS,
-			database		: process.env.GEARHOST_DATABASE
-		});
-		this.pool.on('acquire', (connection) => {
-			console.log('Connection %d aquired.', connection.threadId);
-		});
-		this.pool.on('connection', (connection) => {
-			connection.query('SET SESSION auto_incerement_increment=1');
-		});
-		this.pool.on('enqueue', () => {
-			console.log('Waiting for available connection slot...');
-		});
-		this.pool.on('release', (connection) => {
-			console.log('Connection %d released.', connection.threadId);
-		});
-	};
-
-	stop() {
-		this.pool.end((error) => {
-			if(error) {
-				console.error('Shutdown Error: ' + error.stack);
-			} else {
-				console.log('Shutdown Successfully');	
+		this.knex = require('knex')({
+			client: 'mysql',
+			connection: {
+				host: 		process.env[`GEARHOST_${process.env.ENV}_HOST_URL`],
+				user: 		process.env[`GEARHOST_${process.env.ENV}_USER_NAME`],
+				password: 	process.env[`GEARHOST_${process.env.ENV}_USER_PASS`],
+				database: 	process.env[`GEARHOST_${process.env.ENV}_DATABASE`],
+			},
+			pool: {
+				min: 0,
+				max: 10,
+			},
+			migrations: {
+				directory: './db/migrations',
+				tableName: 'migrations',
+			},
+			seeds: {
+				directory: './db/seeds',
 			}
 		});
 	};
 
-	query(sql, callback) {
-		if(callback === undefined) {
-			callback = (error, results, fields) => {
-				if(error) {
-					console.error('Query Error: ' + error);
-					throw error;
-				} else {
-					return [results, fields];
-				}
-			};
-		}
+	_getTime() {
+		return new Date().toJSON().split(/[A-Z.]/).slice(0,2).join(' ').trim();
+	}
 
-		return this.pool.query(sql, callback);
+	async _getFighters() {
+		return await this.knex.select().from('fighters')
+			.then((results) => { return this._toJSONObject(results); })
+			.catch((error) => { throw error; });
 	};
+
+	async _getPlayers() {
+		return await this.knex.select().from('players')
+			.then((results) => { return this._toJSONObject(results); })
+			.catch((error) => { throw error; });
+	};
+
+	async _getStages() {
+		return await this.knex.select().from('stages')
+			.then((results) => { return this._toJSONObject(results); })
+			.catch((error) => { throw error; });
+	};
+
+	async findFighter(fighterName) {
+		return await this.knex.select().from('fighters').where({ name: fighterName })
+			.then((results) => { return this._toJSONObject(results).id; })
+			.catch((error) => { throw error; });
+	};
+
+	async findOrCreatePlayer(playerName) {
+		return await this.knex.select('id').from('players').where({ name: playerName })
+			.then((results) => { return this._toJSONObject(results).id; })
+			.catch(async (error) => {
+				await this.knex('players').insert({ name: playerName });
+				return await this.knex.select('id').from('players').where({ name: playerName })
+					.then((results) => { return this._toJSONObject(results).id; })
+					.catch((error) => { throw error; });
+			});
+	};
+
+	async findStage(stageName) {
+		return await this.knex.select().from('stages').where({ name: stageName })
+			.then((results) => { return this._toJSONObject(results).id; })
+			.catch((error) => { throw error; });
+	};
+
+	_toJSONObject(object) {
+		let jsonString = JSON.stringify(object);
+		if (object.length <= 1) {
+			jsonString = jsonString.substring(1, jsonString.length - 1);
+			return JSON.parse(jsonString);
+		} else {
+			return object;
+		}
+	}
 };
 
 module.exports = DatabaseClient;
--- db/seeds/setup-fighters.js
+++ db/seeds/setup-fighters.js
@@ -2,7 +2,7 @@ const fighters = require('../../assets/fighters.json');
 
 exports.seed = function(knex, Promise) {
 	// Deletes ALL existing entries
-	return knex('fighters').del().then(function () {
+	return knex.select().from('fighters').del().then(function () {
 		let fightersInsertion = [];
 		let keys = Object.keys(fighters);
 		for (let i = 0; i < keys.length; i++) {
